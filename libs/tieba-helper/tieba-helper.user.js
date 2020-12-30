5a4fcf401da14541c3f10f9f51c696147f5286df
maomao1996 <1714487678@qq.com>
2020-12-30 23:24:22 +0800

    贴吧小助手: 新增广告过滤

--- libs/tieba-helper/tieba-helper.user.js
+++ libs/tieba-helper/tieba-helper.user.js
@@ -1,15 +1,35 @@
 // ==UserScript==
 // @name          贴吧小助手
 // @namespace     https://github.com/maomao1996/tampermonkey-scripts
-// @version       0.1.2
-// @description   自动顶贴回复
+// @version       0.2.0
+// @description   自动顶贴回复、移除贴吧列表和帖子内部广告
 // @author        maomao1996
 // @include       *://tieba.baidu.com/p/*
+// @include       *://tieba.baidu.com/f*
 // @grant         GM_notification
 // ==/UserScript==
 ;
 (function () {
     'use strict';
+    /**
+     * 插件配置
+     **/
+    var CONFIG = {
+        /* 顶帖模块配置项 */
+        // 当前顶贴状态（为 true 时默认执行）
+        STATUS: false,
+        // 顶帖最小间隔（分钟）
+        TIME_MIN: 1,
+        // 顶帖最大间隔（分钟）
+        TIME_MAX: 30,
+        // 顶贴回复内容
+        TEXT: ['顶', '顶~'],
+        // 定时器
+        timer: null,
+        /* 广告模块配置项 */
+        // 广告移除刷新时间
+        REMOVE_REFRESH_TIME: 1000
+    };
     /**
      * 随机函数
      * https://github.com/lodash/lodash/blob/master/random.js
@@ -23,25 +43,24 @@
         return lower + Math.floor(Math.random() * (upper - lower + 1));
     };
     /**
+     * 移除元素
+     */
+    var removeHtmlElement = function (selector) {
+        selector.each(function () {
+            $(this).remove();
+        });
+    };
+    /**
      * 顶帖模块
      **/
-    var DING_CONFIG = {
-        // 当前顶贴状态
-        STATUS: false,
-        // 顶帖最小间隔（分钟）
-        TIME_MIN: 1,
-        // 顶帖最大间隔（分钟）
-        TIME_MAX: 30,
-        // 顶贴回复内容
-        TEXT: ['顶', '顶', '顶'],
-        // 定时器
-        timer: null
-    };
     var autoResponse = function () {
         // 插入控制按钮
-        $('#quick_reply').after('<a id="ding_btn" rel="noopener" class="btn-sub btn-small j_favor">开启自动顶贴回复</a>');
+        $('#quick_reply').after("<a id=\"ding_btn\" rel=\"noopener\" class=\"btn-sub btn-small\">" + (CONFIG.STATUS ? '关闭' : '开启') + "\u81EA\u52A8\u9876\u8D34\u56DE\u590D</a>");
         // 执行顶贴回复
         var runResponse = function () {
+            if (!CONFIG.STATUS) {
+                return;
+            }
             var selectors = {
                 // 输入框选择器
                 editor: '#j_editor_for_container',
@@ -52,11 +71,11 @@
                 var lzlPSelector = '.j_lzl_p.btn-sub.pull-right:visible';
                 // 是否存在一条打开的回复
                 if ($(lzlPSelector).length) {
-                    $(lzlPSelector).trigger('click');
+                    $(lzlPSelector).eq(0).trigger('click');
                 }
                 // 是否打开楼中楼回复
                 else if ($('a.lzl_link_unfold:visible').length) {
-                    $($('.lzl_link_unfold')[0]).trigger('click');
+                    $('a.lzl_link_unfold:visible').eq(0).trigger('click');
                 }
                 // 打开回复楼主
                 else {
@@ -65,23 +84,32 @@
                     selectors.submit = '.j_submit.poster_submit';
                 }
             }
-            var index = random(0, DING_CONFIG.TEXT.length - 1);
-            $(selectors.editor).text(DING_CONFIG.TEXT[index]);
+            var index = random(0, CONFIG.TEXT.length - 1);
+            $(selectors.editor).text(CONFIG.TEXT[index]);
             $(selectors.submit).trigger('click');
-            if (DING_CONFIG.STATUS) {
-                var time = random(DING_CONFIG.TIME_MIN, DING_CONFIG.TIME_MAX, true) * 6e4;
-                console.log(time / 1000 + "\u79D2\u540E\u81EA\u52A8\u9876\u8D34\u56DE\u590D");
-                DING_CONFIG.timer = setTimeout(function () {
-                    runResponse();
-                }, time);
-            }
+            var time = random(CONFIG.TIME_MIN, CONFIG.TIME_MAX, true) * 6e4;
+            console.log(time / 1000 + "\u79D2\u540E\u81EA\u52A8\u9876\u8D34\u56DE\u590D");
+            CONFIG.timer = setTimeout(function () {
+                runResponse();
+            }, time);
         };
+        // 默认是否执行
+        if (CONFIG.STATUS) {
+            setTimeout(function () {
+                runResponse();
+                GM_notification({
+                    text: '已开启自动顶贴回复',
+                    timeout: 2000
+                });
+            }, 1000);
+        }
+        // 顶贴控制函数
         $('#ding_btn').on('click', function () {
-            if (DING_CONFIG.STATUS) {
+            if (CONFIG.STATUS) {
                 // 关闭
-                DING_CONFIG.STATUS = false;
-                clearTimeout(DING_CONFIG.timer);
-                DING_CONFIG.timer = null;
+                CONFIG.STATUS = false;
+                clearTimeout(CONFIG.timer);
+                CONFIG.timer = null;
                 $(this).text('开启自动顶贴回复');
                 GM_notification({
                     text: '已关闭自动顶贴回复',
@@ -90,7 +118,7 @@
             }
             else {
                 // 开启
-                DING_CONFIG.STATUS = true;
+                CONFIG.STATUS = true;
                 $(this).text('关闭自动顶贴回复');
                 GM_notification({
                     text: '已开启自动顶贴回复',
@@ -100,11 +128,26 @@
             }
         });
     };
+    /**
+     * 执行插件
+     **/
     $(window).on('load', function () {
         var pathname = location.pathname;
-        // 自动顶贴回复
+        // 贴子详情
         if (/^\/p\/\d{1,}$/.test(pathname)) {
+            console.log('进入贴子详情');
+            // 自动顶贴回复
             autoResponse();
+            // 移除帖子内部广告
+            removeHtmlElement($('div[ad-dom-img="true"]'));
+        }
+        // 贴吧列表
+        else if (pathname === '/f') {
+            console.log('进入贴吧列表');
+            // 移除贴吧列表广告
+            setInterval(function () {
+                removeHtmlElement($('#thread_list>li').not('.j_thread_list'));
+            }, CONFIG.REMOVE_REFRESH_TIME);
         }
     });
 })();
--- packages/tieba-helper/tieba-helper.user.ts
+++ packages/tieba-helper/tieba-helper.user.ts
@@ -1,15 +1,36 @@
 // ==UserScript==
 // @name          贴吧小助手
 // @namespace     https://github.com/maomao1996/tampermonkey-scripts
-// @version       0.1.2
-// @description   自动顶贴回复
+// @version       0.2.0
+// @description   自动顶贴回复、移除贴吧列表和帖子内部广告
 // @author        maomao1996
 // @include       *://tieba.baidu.com/p/*
+// @include       *://tieba.baidu.com/f*
 // @grant         GM_notification
 // ==/UserScript==
 
 ;(() => {
   'use strict'
+  /**
+   * 插件配置
+   **/
+  const CONFIG = {
+    /* 顶帖模块配置项 */
+    // 当前顶贴状态（为 true 时默认执行）
+    STATUS: false,
+    // 顶帖最小间隔（分钟）
+    TIME_MIN: 1,
+    // 顶帖最大间隔（分钟）
+    TIME_MAX: 30,
+    // 顶贴回复内容
+    TEXT: ['顶', '顶~'],
+    // 定时器
+    timer: null,
+
+    /* 广告模块配置项 */
+    // 广告移除刷新时间
+    REMOVE_REFRESH_TIME: 1000
+  }
 
   /**
    * 随机函数
@@ -28,28 +49,31 @@
   }
 
   /**
+   * 移除元素
+   */
+  const removeHtmlElement = (selector: JQuery): void => {
+    selector.each(function () {
+      $(this).remove()
+    })
+  }
+
+  /**
    * 顶帖模块
    **/
-  const DING_CONFIG = {
-    // 当前顶贴状态
-    STATUS: false,
-    // 顶帖最小间隔（分钟）
-    TIME_MIN: 1,
-    // 顶帖最大间隔（分钟）
-    TIME_MAX: 30,
-    // 顶贴回复内容
-    TEXT: ['顶', '顶', '顶'],
-    // 定时器
-    timer: null
-  }
   const autoResponse = (): void => {
     // 插入控制按钮
     $('#quick_reply').after(
-      '<a id="ding_btn" rel="noopener" class="btn-sub btn-small j_favor">开启自动顶贴回复</a>'
+      `<a id="ding_btn" rel="noopener" class="btn-sub btn-small">${
+        CONFIG.STATUS ? '关闭' : '开启'
+      }自动顶贴回复</a>`
     )
 
     // 执行顶贴回复
     const runResponse = (): void => {
+      if (!CONFIG.STATUS) {
+        return
+      }
+
       const selectors = {
         // 输入框选择器
         editor: '#j_editor_for_container',
@@ -61,11 +85,11 @@
         const lzlPSelector = '.j_lzl_p.btn-sub.pull-right:visible'
         // 是否存在一条打开的回复
         if ($(lzlPSelector).length) {
-          $(lzlPSelector).trigger('click')
+          $(lzlPSelector).eq(0).trigger('click')
         }
         // 是否打开楼中楼回复
         else if ($('a.lzl_link_unfold:visible').length) {
-          $($('.lzl_link_unfold')[0]).trigger('click')
+          $('a.lzl_link_unfold:visible').eq(0).trigger('click')
         }
         // 打开回复楼主
         else {
@@ -75,26 +99,35 @@
         }
       }
 
-      const index = random(0, DING_CONFIG.TEXT.length - 1)
-      $(selectors.editor).text(DING_CONFIG.TEXT[index])
+      const index = random(0, CONFIG.TEXT.length - 1)
+      $(selectors.editor).text(CONFIG.TEXT[index])
       $(selectors.submit).trigger('click')
 
-      if (DING_CONFIG.STATUS) {
-        const time =
-          random(DING_CONFIG.TIME_MIN, DING_CONFIG.TIME_MAX, true) * 6e4
-        console.log(`${time / 1000}秒后自动顶贴回复`)
-        DING_CONFIG.timer = setTimeout(() => {
-          runResponse()
-        }, time)
-      }
+      const time = random(CONFIG.TIME_MIN, CONFIG.TIME_MAX, true) * 6e4
+      console.log(`${time / 1000}秒后自动顶贴回复`)
+      CONFIG.timer = setTimeout(() => {
+        runResponse()
+      }, time)
     }
 
+    // 默认是否执行
+    if (CONFIG.STATUS) {
+      setTimeout(() => {
+        runResponse()
+        GM_notification({
+          text: '已开启自动顶贴回复',
+          timeout: 2000
+        })
+      }, 1000)
+    }
+
+    // 顶贴控制函数
     $('#ding_btn').on('click', function () {
-      if (DING_CONFIG.STATUS) {
+      if (CONFIG.STATUS) {
         // 关闭
-        DING_CONFIG.STATUS = false
-        clearTimeout(DING_CONFIG.timer)
-        DING_CONFIG.timer = null
+        CONFIG.STATUS = false
+        clearTimeout(CONFIG.timer)
+        CONFIG.timer = null
         $(this).text('开启自动顶贴回复')
         GM_notification({
           text: '已关闭自动顶贴回复',
@@ -102,7 +135,7 @@
         })
       } else {
         // 开启
-        DING_CONFIG.STATUS = true
+        CONFIG.STATUS = true
         $(this).text('关闭自动顶贴回复')
         GM_notification({
           text: '已开启自动顶贴回复',
@@ -113,12 +146,29 @@
     })
   }
 
+  /**
+   * 执行插件
+   **/
   $(window).on('load', () => {
     const { pathname } = location
 
-    // 自动顶贴回复
+    // 贴子详情
     if (/^\/p\/\d{1,}$/.test(pathname)) {
+      console.log('进入贴子详情')
+      // 自动顶贴回复
       autoResponse()
+
+      // 移除帖子内部广告
+      removeHtmlElement($('div[ad-dom-img="true"]'))
+    }
+    // 贴吧列表
+    else if (pathname === '/f') {
+      console.log('进入贴吧列表')
+
+      // 移除贴吧列表广告
+      setInterval(function () {
+        removeHtmlElement($('#thread_list>li').not('.j_thread_list'))
+      }, CONFIG.REMOVE_REFRESH_TIME)
     }
   })
 })()
