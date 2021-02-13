7286d981a6f9f34512d5a723c74e18e58442512b
maomao1996 <1714487678@qq.com>
2021-02-13 11:14:19 +0800

    115 小助手: 新增删除空文件夹

--- libs/115-helper/115-helper.user.js
+++ libs/115-helper/115-helper.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name          115小助手
 // @namespace     https://github.com/maomao1996/tampermonkey-scripts
-// @version       0.4.1
-// @description   顶部链接任务入口还原、SHA1 快速查重（新页面打开）、SHA1 查重列表支持选中第一个元素、SHA1 自动查重
+// @version       0.5.0
+// @description   顶部链接任务入口还原、SHA1 快速查重（新页面打开）、SHA1 查重列表支持选中第一个元素、SHA1 自动查重、删除空文件夹
 // @icon      	  https://115.com/favicon.ico
 // @author        maomao1996
 // @include       *://115.com/*
@@ -11,8 +11,8 @@
 ;
 (function () {
     'use strict';
-    // 过滤非 iframe 场景
-    if (window.self === window.top) {
+    // 过滤非 iframe 和 iframe 套娃场景 场景
+    if (window.self === window.top || typeof TOP === 'undefined') {
         return;
     }
     var search = location.search;
@@ -25,32 +25,40 @@
      * 在顶部菜单添加链接任务按钮
      */
     var addLinkTaskBtn = function () {
+        // 避免和其他插件冲突，只添加一次
+        if ($('a.btn-upload[menu="offline_task"]').length) {
+            return;
+        }
         $('#js_top_panel_box .button[menu="upload"]').after('<a href="javascript:;" class="button btn-line btn-upload" menu="offline_task"><i class="icon-operate ifo-linktask"></i><span>链接任务</span><em style="display:none;" class="num-dot"></em></a>');
     };
     /**
-     * 网盘列表 SHA1 查重
+     * 快捷操作增强
+     *  - SHA1查重
+     *  - 删除空文件夹
      */
-    var initRepeatSha1 = function () {
+    var initQuickOperation = function () {
         // 防止重复点击自动查重
         var autoCheckDisabled = false;
-        if (!$('#auto-check-sha1-btn').length) {
-            $('#js_path_add_dir').after('<a id="auto-check-sha1-btn" href="javascript:;" class="button btn-line" style="margin-left: 10px;" title="只查询当前页码目录中的文件"><span>SHA1自动查重</span></a>');
+        // 顶部添加快捷操作按钮
+        if (!$('.mm-quick-operation').length) {
+            $('#js_path_add_dir').after('<a href="javascript:;" class="button btn-line mm-quick-operation" type="auto-sha1" style="margin-left: 10px;" title="只查询当前页码目录中的文件"><span>SHA1自动查重</span></a><a href="javascript:;" class="button btn-line mm-quick-operation" type="delete-empty" style="margin-left: 10px;" title="只删除当前页码目录中的文件夹"><span>删除空文件夹</span></a>');
         }
-        var addRepeatSha1Btn = function () {
-            $('li[file_type="1"]').each(function () {
-                if (!$(this).find('.repeat-sha1-btn').length) {
-                    $(this)
-                        .find('a[menu="public_share"]')
-                        .after('<a class="repeat-sha1-btn"><span>SHA1查重</span></a>');
-                }
-            });
-        };
         var listObserver = new MutationObserver(function (mutationsList) {
+            var isList = $('.list-thumb').length === 0;
             mutationsList.forEach(function (_a) {
                 var type = _a.type;
                 if (type === 'childList') {
                     autoCheckDisabled = false;
-                    addRepeatSha1Btn();
+                    if (!isList) {
+                        return;
+                    }
+                    $('li[file_type="1"]').each(function () {
+                        if (!$(this).find('.mm-operation').length) {
+                            $(this)
+                                .find('a[menu="public_share"]')
+                                .after('<a href="javascript:;" class="mm-operation" type="sha1"><span>SHA1查重</span></a>');
+                        }
+                    });
                 }
             });
         });
@@ -61,7 +69,7 @@
             if (isAll === void 0) { isAll = false; }
             return new Promise(function (resolve) {
                 !isAll &&
-                    MinMessage.Show({ text: '正在查找', type: 'load', timeout: 2e4 });
+                    MinMessage.Show({ text: '正在查找', type: 'load', timeout: 2e5 });
                 TOP.UA$.ajax({
                     url: '//webapi.115.com/files/get_repeat_sha',
                     data: { file_id: file_id },
@@ -72,9 +80,7 @@
                         var state = _a.state, data = _a.data;
                         !isAll && MinMessage.Hide();
                         if (state && data.length > 1) {
-                            GM_openInTab("//115.com/?tab=sha1_repeat&file_id=" + file_id + "&mode=wangpan", {
-                                active: !isAll
-                            });
+                            GM_openInTab("//115.com/?tab=sha1_repeat&file_id=" + file_id + "&mode=wangpan", { active: !isAll });
                             resolve(true);
                         }
                         else {
@@ -90,13 +96,29 @@
                 });
             });
         };
-        $(document).on('click', '.repeat-sha1-btn', function () {
-            var fileId = $(this).parent().parent().attr('file_id');
-            fileId && handleRepeatSha1(fileId);
+        var handleGetDetail = function (aid, cid) {
+            return new Promise(function (resolve) {
+                TOP.Core.DataAccess.Dir.GetDetail(aid, cid, function (_a) {
+                    var size = _a.size;
+                    return resolve(size === '0B');
+                });
+            });
+        };
+        // 单文件操作
+        $(document).on('click', '.mm-operation', function () {
+            var type = $(this).attr('type');
+            var $li = $(this).parents('li');
+            if (!type) {
+                return;
+            }
+            switch (type) {
+                case 'sha1':
+                    return handleRepeatSha1($li.attr('file_id'));
+            }
         });
-        // 一键 SHA1 查重
+        // SHA1 自动查重
         var SHA1_MAP = {};
-        $(document).on('click', '#auto-check-sha1-btn', function () {
+        var handleAutoCheckSha1 = function () {
             if (autoCheckDisabled) {
                 MinMessage.Show({
                     text: '已查询过当前页码所有文件，需再次查询请刷新页面',
@@ -115,7 +137,7 @@
                 });
                 return;
             }
-            MinMessage.Show({ text: '正在查找', type: 'load', timeout: 2e4 });
+            MinMessage.Show({ text: '正在查找', type: 'load', timeout: 2e5 });
             var index = 0;
             // 重复数统计
             var repeatCount = 0;
@@ -149,6 +171,60 @@
                 return findRepeat();
             };
             findRepeat();
+        };
+        // 删除空文件夹
+        var handleDeleteEmptyFolder = function () {
+            var $li = $('li[file_type="0"]');
+            if (!$li.length) {
+                MinMessage.Show({
+                    text: '当前文件目录下没有文件夹',
+                    type: 'war',
+                    timeout: 2e3
+                });
+                return;
+            }
+            MinMessage.Show({ text: '正在查找', type: 'load', timeout: 2e4 });
+            var files = [];
+            $li.each(function () {
+                files.push(handleGetDetail($(this).attr('area_id'), $(this).attr('cate_id')));
+            });
+            Promise.all(files).then(function (result) {
+                var emptyFolderCount = 0;
+                result.forEach(function (item, index) {
+                    if (item) {
+                        emptyFolderCount++;
+                        $li.eq(index).find('[menu="file_check_one"]').trigger('click');
+                    }
+                });
+                if (emptyFolderCount === 0) {
+                    MinMessage.Show({
+                        text: '当前文件目录下没有空文件夹',
+                        type: 'war',
+                        timeout: 2e3
+                    });
+                }
+                else {
+                    MinMessage.Hide();
+                    setTimeout(function () {
+                        $('li[menu="delete"]:visible').trigger('click');
+                    }, 200);
+                }
+            });
+        };
+        // 快捷操作
+        $(document).on('click', '.mm-quick-operation', function () {
+            var type = $(this).attr('type');
+            if (!type) {
+                return;
+            }
+            switch (type) {
+                // SHA1 自动查重
+                case 'auto-sha1':
+                    return handleAutoCheckSha1();
+                // 删除空文件夹
+                case 'delete-empty':
+                    return handleDeleteEmptyFolder();
+            }
         });
     };
     /**
@@ -178,8 +254,8 @@
         if (urlHasString('cid=')) {
             // 添加链接任务入口
             addLinkTaskBtn();
-            // SHA1 查重
-            initRepeatSha1();
+            // 快捷操作初始化
+            initQuickOperation();
         }
         // SHA1 查重列表模块
         else if (urlHasString('tab=sha1_repeat')) {
--- packages/115-helper/115-helper.user.ts
+++ packages/115-helper/115-helper.user.ts
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name          115小助手
 // @namespace     https://github.com/maomao1996/tampermonkey-scripts
-// @version       0.4.1
-// @description   顶部链接任务入口还原、SHA1 快速查重（新页面打开）、SHA1 查重列表支持选中第一个元素、SHA1 自动查重
+// @version       0.5.0
+// @description   顶部链接任务入口还原、SHA1 快速查重（新页面打开）、SHA1 查重列表支持选中第一个元素、SHA1 自动查重、删除空文件夹
 // @icon      	  https://115.com/favicon.ico
 // @author        maomao1996
 // @include       *://115.com/*
@@ -12,8 +12,8 @@
 ;(() => {
   'use strict'
 
-  // 过滤非 iframe 场景
-  if (window.self === window.top) {
+  // 过滤非 iframe 和 iframe 套娃场景 场景
+  if (window.self === window.top || typeof TOP === 'undefined') {
     return
   }
 
@@ -29,39 +29,48 @@
    * 在顶部菜单添加链接任务按钮
    */
   const addLinkTaskBtn = (): void => {
+    // 避免和其他插件冲突，只添加一次
+    if ($('a.btn-upload[menu="offline_task"]').length) {
+      return
+    }
     $('#js_top_panel_box .button[menu="upload"]').after(
       '<a href="javascript:;" class="button btn-line btn-upload" menu="offline_task"><i class="icon-operate ifo-linktask"></i><span>链接任务</span><em style="display:none;" class="num-dot"></em></a>'
     )
   }
 
   /**
-   * 网盘列表 SHA1 查重
+   * 快捷操作增强
+   *  - SHA1查重
+   *  - 删除空文件夹
    */
-  const initRepeatSha1 = (): void => {
+  const initQuickOperation = (): void => {
     // 防止重复点击自动查重
     let autoCheckDisabled = false
 
-    if (!$('#auto-check-sha1-btn').length) {
+    // 顶部添加快捷操作按钮
+    if (!$('.mm-quick-operation').length) {
       $('#js_path_add_dir').after(
-        '<a id="auto-check-sha1-btn" href="javascript:;" class="button btn-line" style="margin-left: 10px;" title="只查询当前页码目录中的文件"><span>SHA1自动查重</span></a>'
+        '<a href="javascript:;" class="button btn-line mm-quick-operation" type="auto-sha1" style="margin-left: 10px;" title="只查询当前页码目录中的文件"><span>SHA1自动查重</span></a><a href="javascript:;" class="button btn-line mm-quick-operation" type="delete-empty" style="margin-left: 10px;" title="只删除当前页码目录中的文件夹"><span>删除空文件夹</span></a>'
       )
     }
 
-    const addRepeatSha1Btn = () => {
-      $('li[file_type="1"]').each(function () {
-        if (!$(this).find('.repeat-sha1-btn').length) {
-          $(this)
-            .find('a[menu="public_share"]')
-            .after('<a class="repeat-sha1-btn"><span>SHA1查重</span></a>')
-        }
-      })
-    }
-
     const listObserver = new MutationObserver((mutationsList) => {
+      const isList = $('.list-thumb').length === 0
       mutationsList.forEach(({ type }) => {
         if (type === 'childList') {
           autoCheckDisabled = false
-          addRepeatSha1Btn()
+          if (!isList) {
+            return
+          }
+          $('li[file_type="1"]').each(function () {
+            if (!$(this).find('.mm-operation').length) {
+              $(this)
+                .find('a[menu="public_share"]')
+                .after(
+                  '<a href="javascript:;" class="mm-operation" type="sha1"><span>SHA1查重</span></a>'
+                )
+            }
+          })
         }
       })
     })
@@ -75,7 +84,7 @@
     ): Promise<boolean> => {
       return new Promise((resolve) => {
         !isAll &&
-          MinMessage.Show({ text: '正在查找', type: 'load', timeout: 2e4 })
+          MinMessage.Show({ text: '正在查找', type: 'load', timeout: 2e5 })
         TOP.UA$.ajax({
           url: '//webapi.115.com/files/get_repeat_sha',
           data: { file_id },
@@ -87,9 +96,7 @@
             if (state && data.length > 1) {
               GM_openInTab(
                 `//115.com/?tab=sha1_repeat&file_id=${file_id}&mode=wangpan`,
-                {
-                  active: !isAll
-                }
+                { active: !isAll }
               )
               resolve(true)
             } else {
@@ -106,14 +113,30 @@
       })
     }
 
-    $(document).on('click', '.repeat-sha1-btn', function () {
-      const fileId = $(this).parent().parent().attr('file_id')
-      fileId && handleRepeatSha1(fileId)
+    const handleGetDetail = (aid: string, cid: string): Promise<boolean> => {
+      return new Promise((resolve) => {
+        TOP.Core.DataAccess.Dir.GetDetail(aid, cid, ({ size }) =>
+          resolve(size === '0B')
+        )
+      })
+    }
+
+    // 单文件操作
+    $(document).on('click', '.mm-operation', function () {
+      const type = $(this).attr('type')
+      const $li = $(this).parents('li')
+      if (!type) {
+        return
+      }
+      switch (type) {
+        case 'sha1':
+          return handleRepeatSha1($li.attr('file_id'))
+      }
     })
 
-    // 一键 SHA1 查重
+    // SHA1 自动查重
     const SHA1_MAP = {}
-    $(document).on('click', '#auto-check-sha1-btn', () => {
+    const handleAutoCheckSha1 = () => {
       if (autoCheckDisabled) {
         MinMessage.Show({
           text: '已查询过当前页码所有文件，需再次查询请刷新页面',
@@ -135,7 +158,7 @@
         return
       }
 
-      MinMessage.Show({ text: '正在查找', type: 'load', timeout: 2e4 })
+      MinMessage.Show({ text: '正在查找', type: 'load', timeout: 2e5 })
 
       let index = 0
       // 重复数统计
@@ -171,6 +194,65 @@
       }
 
       findRepeat()
+    }
+
+    // 删除空文件夹
+    const handleDeleteEmptyFolder = () => {
+      const $li = $('li[file_type="0"]')
+      if (!$li.length) {
+        MinMessage.Show({
+          text: '当前文件目录下没有文件夹',
+          type: 'war',
+          timeout: 2e3
+        })
+        return
+      }
+
+      MinMessage.Show({ text: '正在查找', type: 'load', timeout: 2e4 })
+      const files = []
+      $li.each(function () {
+        files.push(
+          handleGetDetail($(this).attr('area_id'), $(this).attr('cate_id'))
+        )
+      })
+      Promise.all(files).then(function (result) {
+        let emptyFolderCount = 0
+        result.forEach((item, index) => {
+          if (item) {
+            emptyFolderCount++
+            $li.eq(index).find('[menu="file_check_one"]').trigger('click')
+          }
+        })
+
+        if (emptyFolderCount === 0) {
+          MinMessage.Show({
+            text: '当前文件目录下没有空文件夹',
+            type: 'war',
+            timeout: 2e3
+          })
+        } else {
+          MinMessage.Hide()
+          setTimeout(() => {
+            $('li[menu="delete"]:visible').trigger('click')
+          }, 200)
+        }
+      })
+    }
+
+    // 快捷操作
+    $(document).on('click', '.mm-quick-operation', function () {
+      const type = $(this).attr('type')
+      if (!type) {
+        return
+      }
+      switch (type) {
+        // SHA1 自动查重
+        case 'auto-sha1':
+          return handleAutoCheckSha1()
+        // 删除空文件夹
+        case 'delete-empty':
+          return handleDeleteEmptyFolder()
+      }
     })
   }
 
@@ -202,8 +284,8 @@
       // 添加链接任务入口
       addLinkTaskBtn()
 
-      // SHA1 查重
-      initRepeatSha1()
+      // 快捷操作初始化
+      initQuickOperation()
     }
     // SHA1 查重列表模块
     else if (urlHasString('tab=sha1_repeat')) {
