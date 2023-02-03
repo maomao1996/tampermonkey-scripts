613996af2f26d6c633f08a0fa49501d423ec0c38
maomao <1714487678@qq.com>
2023-02-03 16:47:33 +0800

    feat(access-link): 修复跳转链接为站外直链，拦截页面自动跳转

--- packages/access-link/access-link.user.ts
+++ packages/access-link/access-link.user.ts
@@ -1,92 +1,80 @@
 /*!
 // ==UserScript==
-// @name         跳转链接修复
+// @name         跳转链接修复（外链直达）
 // @namespace    https://github.com/maomao1996/tampermonkey-scripts
-// @version      0.4.1
-// @description  为知乎、微信、掘金拦截页面增加跳转按钮（支持3秒后自动跳转）
+// @version      1.0.0
+// @description  修复跳转链接为站外直链，免去拦截页面点击步骤可直达站外；拦截页面自动跳转；已适配知乎、掘金、码云、简书、CSDN
 // @author       maomao1996
-// @include      *://weixin110.qq.com/cgi-bin/mmspamsupport-bin/*
-// @include      *://link.zhihu.com/*
-// @include      *://link.juejin.cn/*
-// @grant        GM_notification
-// @require		   https://cdn.jsdelivr.net/npm/jquery@v3.4.1
+// @include      *
+// @grant        none
 // ==/UserScript==
 */
 
-interface Params {
-  url?: string
-  target?: string
-  [key: string]: any
-}
-
 ;(() => {
   'use strict'
-  function getQueryStringArgs(url: string): Params {
-    if (url && url.indexOf('?') > -1) {
-      const arr = url.split('?')
-      const qs = arr[1]
-      const args: Params = {}
-      const items: string[] = qs.length ? qs.split('&') : []
-      for (let i = 0; i < items.length; i++) {
-        const item = items[i].split('=')
-        const key = decodeURIComponent(item[0])
-        const value = decodeURIComponent(item[1])
-        if (key.length) {
-          args[key] = value
+
+  type Sites = {
+    [key: string]: {
+      /* 类型: 转换 自动跳转 */
+      type?: 'transform' | 'autojump'
+      selector?: string
+      separator?: string
+      query?: string
+    }
+  }
+
+  const SITES: Sites = {
+    /* 知乎 */
+    'www.zhihu.com': {
+      selector: '[href*="link.zhihu.com/?target="]'
+    },
+    'link.zhihu.com': {
+      type: 'autojump'
+    },
+    /* 掘金 */
+    'juejin.cn': {
+      selector: '[href*="link.juejin.cn?target="]'
+    },
+    'link.juejin.cn': {
+      type: 'autojump'
+    },
+    /* 码云 */
+    'gitee.com': {
+      selector: '[href*="gitee.com/link?target="]'
+    },
+    /* 简书 */
+    'www.jianshu.com': {
+      selector: '[href*="links.jianshu.com/go?to="]',
+      separator: 'go?to='
+    },
+    /* CSDN */
+    'link.csdn.net': {
+      type: 'autojump'
+    }
+  }
+
+  const { hostname } = location
+  const {
+    selector,
+    type = 'transform',
+    separator = '?target=',
+    query = 'target'
+  } = SITES[hostname]
+
+  if (type === 'autojump') {
+    /* 自动跳转 */
+    const originUrl = new URLSearchParams(location.search).get(query)
+    originUrl && location.replace(originUrl)
+  } else if (selector) {
+    /* 转换链接 */
+    const observer = new MutationObserver(() => {
+      document.querySelectorAll(selector).forEach((node: HTMLAnchorElement) => {
+        const [, originUrl] = node.href.split(separator)
+        if (originUrl) {
+          node.href = decodeURIComponent(originUrl)
         }
-      }
-      return args
-    }
-    return {}
-  }
-
-  const urlCharRE =
-    /^(https|http):\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9]+[\/=\?%\-&_~`@[\]\':+!]*([^<>\"\"])*$/
-  const params = getQueryStringArgs(location.search)
-  let target: string = ''
-  let url: string = ''
-  let insertion: string = 'after'
-
-  function initParams(u: string, t: string, cls: string = 'button'): string {
-    url = u
-    target = t
-    return (
-      '<a href="' + u + '" class="' + cls + '">继续访问 (3 秒后自动跳转)</a>'
-    )
-  }
-
-  const fns = {
-    'weixin110.qq.com'() {
-      return initParams(
-        $('.weui-msg .weui-msg__desc')
-          .text()
-          .replace(/非微信官方网页，请确认是否继续访问。/, ''),
-        '.weui-msg',
-        'weui-btn_cell weui-btn_cell-primary'
-      )
-    },
-    'link.zhihu.com'() {
-      insertion = 'html'
-      return initParams(params.target!, '.actions')
-    },
-    'link.juejin.cn'() {
-      url = params.target
-      $('button.btn').text('继续访问 (3 秒后自动跳转)')
-    }
-  }
-
-  const fn = fns[location.hostname]
-  const html: string = typeof fn === 'function' ? fn() : ''
-  const isUrl = urlCharRE.test(url)
-
-  if (isUrl) {
-    if (target && html) {
-      $(target)[insertion](html)
-    }
-    setTimeout(() => {
-      location.href = url
-    }, 3000)
-  } else {
-    GM_notification({ timeout: 2e3, text: '获取 url 失败！' })
+      })
+    })
+    observer.observe(document.body, { childList: true, subtree: true })
   }
 })()
