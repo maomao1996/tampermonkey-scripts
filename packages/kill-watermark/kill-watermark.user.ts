ae0d3ea70fc3ef722bf4f0ecc68a6f8fd6b59f10
maomao <1714487678@qq.com>
2023-02-10 18:26:37 +0800

    fix(kill-watermark): 修复稿定设计

--- packages/kill-watermark/kill-watermark.user.ts
+++ packages/kill-watermark/kill-watermark.user.ts
@@ -2,19 +2,23 @@
 // ==UserScript==
 // @name          杀死水印（Kill Watermark）
 // @namespace     https://github.com/maomao1996/tampermonkey-scripts
-// @version       0.0.1
+// @version       0.0.2
 // @description   杀死水印（移除烦人的水印，还你一个干净清爽的页面）；已适配稿定设计、腾讯视频
 // @author        maomao1996
-// @match         *://v.qq.com/x/cover/*
 // @match         *://*.gaoding.com/*
+// @match         *://v.qq.com/x/cover/*
 // @grant         GM_addStyle
 // ==/UserScript==
 */
 
 ;(() => {
   'use strict'
-  let css = ''
   const selectors = [
+    /**
+     * 稿定设计
+     */
+    '.editor-watermark',
+
     /**
      * 腾讯视频右上角 logo
      * https://v.qq.com/x/cover/mzc0020027yzd9e/q0043cz9x20.html
@@ -22,11 +26,10 @@
     'txpdiv.txp-watermark'
   ]
 
-  /**
-   * 稿定设计
-   */
-  css += `.editor-watermark {z-index: -1 !important}`
-
-  css += `${selectors} {display: none !important}`
-  GM_addStyle(css)
+  const rules = [
+    'display: none !important',
+    'z-index: -999 !important',
+    'filter: opacity(0) !important'
+  ]
+  GM_addStyle(`${selectors} {${rules.join(';')}}`)
 })()
