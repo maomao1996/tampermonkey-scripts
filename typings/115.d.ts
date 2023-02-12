8e92e8042165ab0093c50b6484d9197d0cef9206
maomao <1714487678@qq.com>
2023-02-12 23:21:51 +0800

    refactor: 优化 ts 类型声明

---
+++ typings/115.d.ts
@@ -0,0 +1,24 @@
+/**
+ * 115 相关类型声明
+ */
+declare const TOP: Window
+
+declare const checkRepaatApi: any
+
+interface Window {
+  $: JQueryStatic
+  Core: any
+  UA$: { ajax(options: JQuery.AjaxSettings): void }
+  USER_INFO: {
+    ALIAS_NAME: string
+    IS_VIP: number
+    USER_ID: string
+    USER_NAME: string
+  }
+  Ext: any
+}
+
+interface JQueryStatic {
+  confirm(...args: any[]): void
+  alertTip(...args: any[]): void
+}
--- typings/tampermonkey.d.ts
+++ typings/tampermonkey.d.ts
@@ -1,27 +1,31 @@
 /**
+ * tampermonkey 相关类型声明
+ */
+
+/**
  * 添加指定样式添加到文档中，并返回注入的样式元素
- **/
+ */
 declare function GM_addStyle(css: string): HTMLStyleElement
 
 /**
  * 标签页通知
- **/
+ */
 declare function GM_notification(details: {
   /**
    * 通知的标题 默认读取脚本名
-   **/
+   */
   title?: string
   /**
    * 通知的文本
-   **/
+   */
   text: string
   /**
    * 通知的图片
-   **/
+   */
   image?: string
   /**
    * 是否突出显示
-   **/
+   */
   highlight?: boolean
   /**
    * 是否播放声音
@@ -29,42 +33,42 @@ declare function GM_notification(details: {
   silent?: boolean
   /**
    * 通知显示的时长 0=禁用
-   **/
+   */
   timeout?: number
   /**
    * 在关闭通知时调用
-   **/
+   */
   ondone?(): void
   /**
    * 超时或单击通知时调用
-   **/
+   */
   onclick?(): void
 }): void
 
 /**
  * 打开一个新的浏览器标签页
- **/
+ */
 declare function GM_openInTab(
   /**
    * 打开指定的页面的URL
-   **/
+   */
   url: string,
   options?: {
     /**
      * 是否聚焦到新标签页(默认 false)
-     **/
+     */
     active?: boolean
     /**
      * 是否在当前标签页之后插入(默认 true)
-     **/
+     */
     insert?: boolean
     /**
      * 使浏览器将当前标签重新聚焦在关闭位置上
-     **/
+     */
     setParent?: boolean
     /**
      * 使标签页在无痕模式下打开
-     **/
+     */
     incognito?: boolean
   }
 ): void
@@ -75,11 +79,11 @@ declare function GM_openInTab(
 declare function GM_registerMenuCommand(
   /**
    * 菜单名称
-   **/
+   */
   name: string,
   /**
    * 点击菜单的回调
-   **/
+   */
   fn?: () => void,
   accessKey?: string
 ): number
@@ -95,30 +99,5 @@ interface GMConfig {
 }
 /**
  * 脚本设置
- **/
+ */
 declare const GM_config: GMConfig
-
-/**
- * 115 相关
- **/
-declare const TOP: Window
-
-declare const checkRepaatApi: any
-
-interface Window {
-  $: JQueryStatic
-  Core: any
-  UA$: { ajax(options: JQuery.AjaxSettings): void }
-  USER_INFO: {
-    ALIAS_NAME: string
-    IS_VIP: number
-    USER_ID: string
-    USER_NAME: string
-  }
-  Ext: any
-}
-
-interface JQueryStatic {
-  confirm(...args: any[]): void
-  alertTip(...args: any[]): void
-}
