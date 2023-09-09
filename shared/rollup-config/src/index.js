55f98e88b85d8f2c05ef292a7ea9ad3d93114ca0
maomao <1714487678@qq.com>
2023-09-09 13:50:50 +0800

    feat(rollup-config): 添加 rollup 公共配置

---
+++ shared/rollup-config/src/index.js
@@ -0,0 +1,51 @@
+import path from 'node:path'
+import { defineConfig } from 'rollup'
+import postcss from 'rollup-plugin-postcss'
+import { swc } from 'rollup-plugin-swc3'
+import terser from '@rollup/plugin-terser'
+import metablock from 'rollup-plugin-userscript-metablock'
+
+export function createRollupConfig({ pkg }) {
+  const file = path.resolve(
+    '../../',
+    process.env.BUILD === 'development' ? 'dist-dev' : 'dist',
+    `${pkg.name}.user.js`,
+  )
+
+  return defineConfig({
+    input: 'src/index.ts',
+    output: {
+      file,
+      format: 'iife',
+    },
+    plugins: [
+      postcss({ minimize: true }),
+      swc({
+        jsc: { target: 'es5' },
+      }),
+      terser({
+        mangle: {
+          keep_fnames: true,
+        },
+        compress: {
+          defaults: false,
+        },
+        format: {
+          ascii_only: true,
+          beautify: true,
+          indent_level: 2,
+        },
+      }),
+      metablock({
+        override: {
+          version: pkg.version,
+          namespace: 'https://github.com/maomao1996/tampermonkey-scripts',
+          author: 'maomao1996',
+          homepage: 'https://github.com/maomao1996/tampermonkey-scripts',
+          supportURL: 'https://github.com/maomao1996/tampermonkey-scripts/issues',
+          license: 'MIT',
+        },
+      }),
+    ],
+  })
+}
