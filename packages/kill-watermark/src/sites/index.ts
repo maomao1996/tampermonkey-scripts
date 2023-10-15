d5b57f7a4aac2e002c8485ea7ce8db04e8e8022c
maomao <1714487678@qq.com>
2023-10-15 13:38:47 +0800

    feat(kill-watermark): 适配 CSDN C 知道

--- packages/kill-watermark/src/sites/index.ts
+++ packages/kill-watermark/src/sites/index.ts
@@ -3,6 +3,7 @@ import { default as gaodingCom } from './gaoding.com.css'
 import { default as iqiyiCom } from './iqiyi.com.css'
 import { default as keQqCom } from './ke.qq.com.css'
 import { default as liveBilibiliCom } from './live.bilibili.com.css'
+import { default as soCsdnNet } from './so.csdn.net.css'
 import { default as vQqCom } from './v.qq.com.css'
 import { default as vYoukuCom } from './v.youku.com.css'
 import { default as yuqueCom } from './yuque.com.css'
@@ -13,6 +14,7 @@ export default [
   ['iqiyi.com', iqiyiCom],
   ['ke.qq.com', keQqCom],
   ['live.bilibili.com', liveBilibiliCom],
+  ['so.csdn.net', soCsdnNet],
   ['v.qq.com', vQqCom],
   ['v.youku.com', vYoukuCom],
   ['yuque.com', yuqueCom],
