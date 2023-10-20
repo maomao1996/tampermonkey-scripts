d5debbc72862b6d4e41e639c7b251c5f7c519a3d
maomao <1714487678@qq.com>
2023-10-20 15:41:49 +0800

    fix(remove-redirect): 修复码云个人主页

---
+++ .changeset/pink-frogs-live.md
@@ -0,0 +1,5 @@
+---
+'remove-redirect': patch
+---
+
+修复码云个人主页
--- packages/remove-redirect/src/sites/gitee.com.ts
+++ packages/remove-redirect/src/sites/gitee.com.ts
@@ -1,5 +1,6 @@
 /******************************************************************************
  ** 码云
+ **   - https://gitee.com/femm
  **   - https://gitee.com/mirrors/vue-mmplayer
  **   - https://gitee.com/link?target=https%3A%2F%2Ffe-mm.com
  **   - https://gitee.com/link?target=https%3A%2F%2Fgithub.com%2Fmaomao1996%2Ftampermonkey-scripts
@@ -10,7 +11,8 @@ const sites: SiteModule = [
     'gitee.com',
     {
       transform: {
-        selector: '[href*="gitee.com/link?target="]',
+        // 个人主页没有 /gitee.com 前缀
+        selector: '[href*="/link?target="]',
       },
       autojump: {
         validator: ({ pathname }) => pathname === '/link',
