462b407e30b24083fb3ac0fc47447ad4fe9477b3
maomao <1714487678@qq.com>
2023-10-22 15:21:26 +0800

    feat(remove-redirect): 适配 CSDN 文章页

--- packages/remove-redirect/src/sites/csdn.net.ts
+++ packages/remove-redirect/src/sites/csdn.net.ts
@@ -1,9 +1,19 @@
 /******************************************************************************
  ** CSDN
+ **   - https://blog.csdn.net/LoseInVain/article/details/122735603
  **   - https://link.csdn.net/?target=https%3A%2F%2Ffe-mm.com
  **   - https://link.csdn.net/?target=https%3A%2F%2Fgithub.com%2Fmaomao1996%2Ftampermonkey-scripts
  ******************************************************************************/
 const sites: SiteModule = [
+  [
+    ,
+    'blog.csdn.net',
+    {
+      rewriteWindowOpen: {
+        validationRule: 'link.csdn.net?target=',
+      },
+    },
+  ],
   [
     'CSDN',
     'link.csdn.net',
