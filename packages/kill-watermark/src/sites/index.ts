62210a3b5f98455082b70ff85787760d611dec8f
maomao <1714487678@qq.com>
2023-10-23 19:35:55 +0800

    feat(kill-watermark): 适配飞书

--- packages/kill-watermark/src/sites/index.ts
+++ packages/kill-watermark/src/sites/index.ts
@@ -1,4 +1,5 @@
 import { default as docsQqCom } from './docs.qq.com.css'
+import { default as feishuCn } from './feishu.cn.css'
 import { default as gaodingCom } from './gaoding.com.css'
 import { default as iqiyiCom } from './iqiyi.com.css'
 import { default as keQqCom } from './ke.qq.com.css'
@@ -10,6 +11,7 @@ import { default as yuqueCom } from './yuque.com.css'
 
 export default [
   ['docs.qq.com', docsQqCom],
+  ['feishu.cn', feishuCn],
   ['gaoding.com', gaodingCom],
   ['iqiyi.com', iqiyiCom],
   ['ke.qq.com', keQqCom],
