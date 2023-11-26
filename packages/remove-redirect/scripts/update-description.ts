d49bc2003b71010935abe1a23da33c59a03255c4
maomao <1714487678@qq.com>
2023-11-26 11:40:43 +0800

    chore(remove-redirect): 添加 description 更新脚本

---
+++ packages/remove-redirect/scripts/update-description.ts
@@ -0,0 +1,17 @@
+import * as path from 'node:path'
+
+import fs from 'fs-extra'
+
+import * as sites from 'src/sites'
+
+const description = Object.values(sites)
+  .flat()
+  .map(([name]) => name)
+  .filter(Boolean)
+  .join('、')
+
+const filepath = path.join(process.cwd(), 'metablock.json')
+const metablock = fs.readJSONSync(filepath)
+metablock.description = `修复跳转链接为站外直链（移除重定向），免去拦截页面点击步骤可直达站外；拦截页面自动跳转；已适配${description}`
+
+fs.writeJSONSync(filepath, metablock, { spaces: 2 })
