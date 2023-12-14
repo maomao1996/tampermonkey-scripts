99bc29fc08d981414def6295eb092184d0fa9fe3
maomao <1714487678@qq.com>
2023-12-14 18:30:46 +0800

    fix(remove-redirect): 修复 YouTube 拦截页

---
+++ .changeset/sour-bags-explain.md
@@ -0,0 +1,5 @@
+---
+'remove-redirect': patch
+---
+
+修复 YouTube 拦截页
--- packages/remove-redirect/src/sites/youtube.com.ts
+++ packages/remove-redirect/src/sites/youtube.com.ts
@@ -1,6 +1,7 @@
 /******************************************************************************
  ** YouTube
  **   - https://www.youtube.com/watch?v=c5vGiaTudPc
+ **   - https://www.youtube.com/redirect?&q=https%3A%2F%2Fgithub.com%2Fmaomao1996%2Ftampermonkey-scripts
  ******************************************************************************/
 const sites: SiteModule = [
   [
@@ -9,6 +10,10 @@ const sites: SiteModule = [
     {
       transform: {
         selector: '[href*="youtube.com/redirect?event="]',
+        queryName: 'q',
+      },
+      autojump: {
+        validator: ({ pathname }) => pathname === '/redirect',
         queryName: 'q',
       },
     },
