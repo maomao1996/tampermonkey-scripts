6273aab0a34c40568d2dfbb8f411d7e1b55273ee
maomao <1714487678@qq.com>
2023-12-23 13:03:07 +0800

    feat(remove-redirect): 适配 Instagram (#15)

---
+++ packages/remove-redirect/src/sites/instagram.com.ts
@@ -0,0 +1,19 @@
+/******************************************************************************
+ ** Instagram
+ **   - https://www.instagram.com/linkshim/?u=https%3A%2F%2Fgithub.com%2Fmaomao1996%2Ftampermonkey-scripts&e=AT3Mdb540GAp6eHsU0Z_QGrlAc5-VgTKBSWUnq0lwzTDCfOTdT-GR2LIFutHYByEP2A0ql_mLpeU9oe1IuxVdisH-sh2AO2xwj6br5GuXjDODuX8zrwUIjYbkiya3t61XE3p3io
+ **   - https://l.instagram.com/?u=https%3A%2F%2Fgithub.com%2Fmaomao1996%2Ftampermonkey-scripts&e=AT3Mdb540GAp6eHsU0Z_QGrlAc5-VgTKBSWUnq0lwzTDCfOTdT-GR2LIFutHYByEP2A0ql_mLpeU9oe1IuxVdisH-sh2AO2xwj6br5GuXjDODuX8zrwUIjYbkiya3t61XE3p3io
+ ******************************************************************************/
+const sites: SiteModule = [
+  [
+    'Instagram',
+    /^(?:l\.)?instagram\.com$/,
+    {
+      autojump: {
+        validator: ({ search }) => search.includes('u='),
+        queryName: 'u',
+      },
+    },
+  ],
+]
+
+export default sites
