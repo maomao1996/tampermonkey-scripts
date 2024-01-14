42641d69865ef271bb55c7be74d643c9cdf9aaf9
maomao1996 <1714487678@qq.com>
2024-01-14 05:42:10 +0000

    Deploying to gh-pages from @ maomao1996/tampermonkey-scripts@fd0bfc4e815240cddf76df53c058efeffa6c5ef4 🚀

--- kill-watermark.user.js
+++ kill-watermark.user.js
@@ -2,7 +2,7 @@
 // @name        杀死水印（Kill Watermark）
 // @description 杀死水印（移除烦人的水印，还你一个干净清爽的页面）；已适配360 智脑、腾讯文档、飞书、FreeBuf 网络安全行业门户、稿定设计、爱奇艺播放页（右上角 logo、暂停时的广告）、腾讯课堂播放页漂浮水印、哔哩哔哩直播左上角 logo、CSDN C 知道、腾讯视频播放页（右上角 logo、暂停时的弹窗广告）、优酷视频播放页（右上角 logo、暂停时的弹窗广告）、语雀
 // @namespace   maomao1996.kill-watermark
-// @version     0.9.1
+// @version     0.9.2
 // @author      maomao1996
 // @homepage    https://github.com/maomao1996/tampermonkey-scripts
 // @supportURL  https://github.com/maomao1996/tampermonkey-scripts/issues
@@ -22,20 +22,21 @@
 // ==/UserScript==
 !function() {
   "use strict";
-  var t = "undefined" != typeof window ? location.hostname.replace(/^www\./, "") : "", e = [ "360 \u667a\u8111", "chat.360.com", {
+  var t = "undefined" != typeof window;
+  var e = [ "360 \u667a\u8111", "chat.360.com", {
     style: '#nworld-app-container div>div[style*=pointer-events][style*="data:image/"]{display:none!important}'
   } ], o = [ "\u817e\u8baf\u6587\u6863", "docs.qq.com", {
     style: ".watermark-bg-wrapper{display:none!important}"
   } ], r = [ "\u98de\u4e66", "feishu.cn", {
     style: ".print-watermark[style],.ssrWaterMark[style],.suite-clear[style]{display:none!important;height:0!important;width:0!important}"
-  } ], i = [ "FreeBuf \u7f51\u7edc\u5b89\u5168\u884c\u4e1a\u95e8\u6237", "freebuf.com", {
+  } ], n = [ "FreeBuf \u7f51\u7edc\u5b89\u5168\u884c\u4e1a\u95e8\u6237", "freebuf.com", {
     script: function() {
       document.querySelectorAll("img[large]").forEach((function(t) {
         var e = t.getAttribute("large");
         e && t.src !== e && (t.src = e);
       }));
     }
-  } ], n = [ "\u7a3f\u5b9a\u8bbe\u8ba1", "gaoding.com", {
+  } ], i = [ "\u7a3f\u5b9a\u8bbe\u8ba1", "gaoding.com", {
     style: '.editor-canvas div[style*="pointer-events: none !important"][style*="visibility: visible !important"][style*=background]{clip-path:circle(0)!important}'
   } ], a = [ "\u7231\u5947\u827a\u64ad\u653e\u9875\uff08\u53f3\u4e0a\u89d2 logo\u3001\u6682\u505c\u65f6\u7684\u5e7f\u544a\uff09", "iqiyi.com", {
     style: ".iqp-logo-bottom,.iqp-logo-box,.iqp-logo-top,iqpdiv.flash-max{display:none!important}iqpdiv.iqp-player-videolayer{height:100%!important;left:auto!important;top:auto!important;width:100%!important}"
@@ -45,26 +46,26 @@
     style: ".web-player-icon-roomStatus{opacity:0!important}"
   } ], p = [ "CSDN C \u77e5\u9053", "so.csdn.net", {
     style: ".username_mask_cover[style]{display:none!important}"
-  } ], y = [ "\u817e\u8baf\u89c6\u9891\u64ad\u653e\u9875\uff08\u53f3\u4e0a\u89d2 logo\u3001\u6682\u505c\u65f6\u7684\u5f39\u7a97\u5e7f\u544a\uff09", "v.qq.com", {
+  } ], u = [ "\u817e\u8baf\u89c6\u9891\u64ad\u653e\u9875\uff08\u53f3\u4e0a\u89d2 logo\u3001\u6682\u505c\u65f6\u7684\u5f39\u7a97\u5e7f\u544a\uff09", "v.qq.com", {
     style: "txpdiv.txp-watermark{opacity:0!important}[data-role=creative-player-pause-layer]{display:none!important}"
-  } ], c = [ "\u4f18\u9177\u89c6\u9891\u64ad\u653e\u9875\uff08\u53f3\u4e0a\u89d2 logo\u3001\u6682\u505c\u65f6\u7684\u5f39\u7a97\u5e7f\u544a\uff09", "v.youku.com", {
+  } ], y = [ "\u4f18\u9177\u89c6\u9891\u64ad\u653e\u9875\uff08\u53f3\u4e0a\u89d2 logo\u3001\u6682\u505c\u65f6\u7684\u5f39\u7a97\u5e7f\u544a\uff09", "v.youku.com", {
     style: "watermark-layer{opacity:0!important}#youku-pause-container{display:none!important}"
-  } ], u = [ "\u8bed\u96c0", "yuque.com", {
+  } ], c = [ "\u8bed\u96c0", "yuque.com", {
     style: "#main>div.wm{display:none!important}"
   } ], m = Object.freeze({
     __proto__: null,
     chat360Com: e,
     docsQqCom: o,
     feishuCn: r,
-    freebufCom: i,
-    gaodingCom: n,
+    freebufCom: n,
+    gaodingCom: i,
     iqiyiCom: a,
     keQqCom: l,
     liveBilibiliCom: s,
     soCsdnNet: p,
-    vQqCom: y,
-    vYoukuCom: c,
-    yuqueCom: u
+    vQqCom: u,
+    vYoukuCom: y,
+    yuqueCom: c
   });
   function d(t, e) {
     (null == e || e > t.length) && (e = t.length);
@@ -77,19 +78,19 @@
     }(t) || function(t, e) {
       var o = null == t ? null : "undefined" != typeof Symbol && t[Symbol.iterator] || t["@@iterator"];
       if (null != o) {
-        var r, i, n = [], a = !0, l = !1;
+        var r, n, i = [], a = !0, l = !1;
         try {
-          for (o = o.call(t); !(a = (r = o.next()).done) && (n.push(r.value), !e || n.length !== e); a = !0) ;
+          for (o = o.call(t); !(a = (r = o.next()).done) && (i.push(r.value), !e || i.length !== e); a = !0) ;
         } catch (t) {
-          l = !0, i = t;
+          l = !0, n = t;
         } finally {
           try {
             a || null == o.return || o.return();
           } finally {
-            if (l) throw i;
+            if (l) throw n;
           }
         }
-        return n;
+        return i;
       }
     }(t, e) || function(t, e) {
       if (!t) return;
@@ -102,12 +103,15 @@
       throw new TypeError("Invalid attempt to destructure non-iterable instance.\\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");
     }();
   }
-  var v = Object.values(m).find((function(e) {
-    var o = f(e, 2)[1];
-    return "string" == typeof o ? t.includes(o) : o.test(t);
+  var v = function() {
+    var e = arguments.length > 0 && void 0 !== arguments[0] ? arguments[0] : location.hostname;
+    return t ? e.replace(/^www\./, "") : "";
+  }(), g = Object.values(m).find((function(t) {
+    var e = f(t, 2)[1];
+    return "string" == typeof e ? v.includes(e) : e.test(v);
   }));
-  if (v) {
-    var g = v[2], b = g.style, h = g.script;
-    b && GM_addStyle(b), "function" == typeof h && h();
+  if (g) {
+    var b = g[2], h = b.style, q = b.script;
+    h && GM_addStyle(h), "function" == typeof q && q();
   }
 }();
--- remove-redirect.user.js
+++ remove-redirect.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name        跳转链接修复（移除重定向外链直达）
-// @description 修复跳转链接为站外直链（移除重定向），免去拦截页面点击步骤可直达站外；拦截页面自动跳转；已适配爱发电、百度、NGA 玩家社区、CSDN、豆瓣、码云、谷歌搜索、花瓣网、InfoQ、简书、掘金、金山文档、力扣（Leetcode）、51CTO 博客、牛客网、开源中国、pixiv、微信、微信开放社区、QQ 邮箱、PC 版 QQ、腾讯文档、360 搜索、少数派、腾讯云开发者社区、微博、YouTube、语雀、知乎、知乎专栏
+// @description 修复跳转链接为站外直链（移除重定向），免去拦截页面点击步骤可直达站外；拦截页面自动跳转；已适配爱发电、百度、百度移动端、NGA 玩家社区、CSDN、豆瓣、Facebook、码云、谷歌搜索、花瓣网、InfoQ、Instagram、简书、掘金、金山文档、力扣（Leetcode）、51CTO 博客、牛客网、开源中国、pixiv、微信、微信开放社区、QQ 邮箱、PC 版 QQ、腾讯文档、腾讯兔小巢、360 搜索、少数派、腾讯云开发者社区、微博、YouTube、语雀、知乎、知乎专栏
 // @namespace   maomao1996.remove-redirect
-// @version     2.4.0
+// @version     2.4.1
 // @author      maomao1996
 // @homepage    https://github.com/maomao1996/tampermonkey-scripts
 // @supportURL  https://github.com/maomao1996/tampermonkey-scripts/issues
@@ -16,48 +16,48 @@
 // ==/UserScript==
 !function() {
   "use strict";
-  var e = Array.isArray, t = function(e) {
-    return "function" == typeof e;
-  }, r = function(e) {
-    return "string" == typeof e;
-  }, o = "undefined" != typeof window ? location.hostname.replace(/^www\./, "") : "";
-  function n(e) {
+  var t = Array.isArray, e = function(t) {
+    return "function" == typeof t;
+  }, r = function(t) {
+    return "string" == typeof t;
+  }, n = "undefined" != typeof window;
+  function o(t) {
     if (!r("string")) return !1;
     try {
-      return new URL(e), !0;
-    } catch (e) {
+      return new URL(t), !0;
+    } catch (t) {
       return !1;
     }
   }
   var a = [ [ "\u767e\u5ea6", "baidu.com", {
     transform: {
       selector: "#content_left > [mu]",
-      customTransform: function(e) {
-        var t = e.getAttribute("mu");
-        n(t) && !t.includes(".baidu.com") && e.querySelectorAll("a[href]").forEach((function(e) {
-          return e.setAttribute("href", t);
+      customTransform: function(t) {
+        var e = t.getAttribute("mu");
+        o(e) && !e.includes(".baidu.com") && t.querySelectorAll("a[href]").forEach((function(t) {
+          return t.setAttribute("href", e);
         }));
       }
     }
-  } ] ], u = [ "onclick", "onmouseover", "onmouseout" ], i = [ [ "NGA \u73a9\u5bb6\u793e\u533a", /^(bbs\.nga\.cn|ngabbs\.com|g\.nga\.cn)$/, {
-    transform: {
-      selector: 'a[target="_blank"][onclick*="showUrlAlert"]',
-      customTransform: function(e) {
-        u.forEach((function(t) {
-          return e.removeAttribute(t);
-        }));
-      }
-    }
-  } ] ], c = [ [ "\u8c37\u6b4c\u641c\u7d22", /^google\.com/, {
+  } ] ], u = [ [ "\u8c37\u6b4c\u641c\u7d22", /^google\.com/, {
     transform: {
       selector: [ "a[jsname][href][data-jsarwt]", "a[jsname][href][ping]" ].join(","),
-      customTransform: function(e) {
-        e.setAttribute("data-jrwt", "1"), e.removeAttribute("ping");
-        var t = (e.getAttribute("href") || "").match(/\?(.*)/);
-        if (t) {
-          var r = new URLSearchParams(t[1]).get("url");
-          r && n(r) && e.setAttribute("href", r);
+      customTransform: function(t) {
+        t.setAttribute("data-jrwt", "1"), t.removeAttribute("ping");
+        var e = (t.getAttribute("href") || "").match(/\?(.*)/);
+        if (e) {
+          var r = new URLSearchParams(e[1]).get("url");
+          r && o(r) && t.setAttribute("href", r);
         }
+      }
+    }
+  } ] ], i = [ "onclick", "onmouseover", "onmouseout" ], c = [ [ "NGA \u73a9\u5bb6\u793e\u533a", /^(bbs\.nga\.cn|ngabbs\.com|g\.nga\.cn)$/, {
+    transform: {
+      selector: 'a[target="_blank"][onclick*="showUrlAlert"]',
+      customTransform: function(t) {
+        i.forEach((function(e) {
+          return t.removeAttribute(e);
+        }));
       }
     }
   } ] ], m = [ [ "\u725b\u5ba2\u7f51", "nowcoder.com", {
@@ -69,8 +69,8 @@
     autojump: {}
   } ] ], l = [ [ "\u5fae\u4fe1", "weixin110.qq.com", {
     autojump: {
-      validator: function(e) {
-        return "/cgi-bin/mmspamsupport-bin/newredirectconfirmcgi" === e.pathname;
+      validator: function(t) {
+        return "/cgi-bin/mmspamsupport-bin/newredirectconfirmcgi" === t.pathname;
       },
       getOriginalUrl: function() {
         return document.querySelector(".weui-msg p.weui-msg__desc").textContent;
@@ -79,39 +79,39 @@
     }
   } ], [ "\u5fae\u4fe1\u5f00\u653e\u793e\u533a", "developers.weixin.qq.com", {
     autojump: {
-      validator: function(e) {
-        return "/community/middlepage/href" === e.pathname;
+      validator: function(t) {
+        return "/community/middlepage/href" === t.pathname;
       },
       queryName: "href"
     }
   } ], [ "QQ \u90ae\u7bb1", "mail.qq.com", {
     autojump: {
-      validator: function(e) {
-        return "/cgi-bin/readtemplate" === e.pathname;
+      validator: function(t) {
+        return "/cgi-bin/readtemplate" === t.pathname;
       },
       selector: "div.c-footer a.c-footer-a1",
       queryName: "gourl"
     }
   } ], [ "PC \u7248 QQ", "c.pc.qq.com", {
     autojump: {
-      validator: function(e) {
-        return "/middlem.html" === e.pathname;
+      validator: function(t) {
+        return "/middlem.html" === t.pathname;
       },
       queryName: "pfurl"
     }
   } ], [ "\u817e\u8baf\u6587\u6863", "docs.qq.com", {
     autojump: {
-      validator: function(e) {
-        return "/scenario/link.html" === e.pathname;
+      validator: function(t) {
+        return "/scenario/link.html" === t.pathname;
       },
       queryName: "url"
     }
   } ] ], s = [ [ "360 \u641c\u7d22", "so.com", {
     transform: {
       selector: 'a[href*="so.com/link?"][data-mdurl]',
-      customTransform: function(e) {
-        var t = e.getAttribute("data-mdurl");
-        n(t) && e.setAttribute("href", t);
+      customTransform: function(t) {
+        var e = t.getAttribute("data-mdurl");
+        o(e) && t.setAttribute("href", e);
       }
     }
   } ] ], f = Object.freeze({
@@ -121,13 +121,12 @@
         selector: '[href*="afdian.net/link?target="]'
       },
       autojump: {
-        validator: function(e) {
-          return "/link" === e.pathname;
+        validator: function(t) {
+          return "/link" === t.pathname;
         }
       }
     } ] ],
     baiduCom: a,
-    bbsNgaCn: i,
     csdnNet: [ [ , "blog.csdn.net", {
       rewriteWindowOpen: {
         validationRule: "link.csdn.net?target="
@@ -137,16 +136,16 @@
     } ] ],
     doubanCom: [ [ "\u8c46\u74e3", "douban.com", {
       autojump: {
-        validator: function(e) {
-          return "/link2/" === e.pathname;
+        validator: function(t) {
+          return "/link2/" === t.pathname;
         },
         queryName: "url"
       }
     } ] ],
     facebookCom: [ [ "Facebook", /^(?:l\.)?facebook\.com$/, {
       autojump: {
-        validator: function(e) {
-          return e.search.includes("u=");
+        validator: function(t) {
+          return t.search.includes("u=");
         },
         queryName: "u"
       }
@@ -156,16 +155,16 @@
         selector: '[href*="/link?target="]'
       },
       autojump: {
-        validator: function(e) {
-          return "/link" === e.pathname;
+        validator: function(t) {
+          return "/link" === t.pathname;
         }
       }
     } ] ],
-    googleCom: c,
+    googleCom: u,
     huabanCom: [ [ "\u82b1\u74e3\u7f51", "huaban.com", {
       autojump: {
-        validator: function(e) {
-          return "/go" === e.pathname;
+        validator: function(t) {
+          return "/go" === t.pathname;
         },
         selector: ".wrapper button.ant-btn"
       }
@@ -175,15 +174,15 @@
         validationRule: "infoq.cn/link?target="
       },
       autojump: {
-        validator: function(e) {
-          return "/link" === e.pathname;
+        validator: function(t) {
+          return "/link" === t.pathname;
         }
       }
     } ] ],
     instagramCom: [ [ "Instagram", /^(?:l\.)?instagram\.com$/, {
       autojump: {
-        validator: function(e) {
-          return e.search.includes("u=");
+        validator: function(t) {
+          return t.search.includes("u=");
         },
         queryName: "u"
       }
@@ -194,8 +193,8 @@
         separator: "go?to="
       },
       autojump: {
-        validator: function(e) {
-          return "/go-wild" === e.pathname;
+        validator: function(t) {
+          return "/go-wild" === t.pathname;
         },
         queryName: "url"
       }
@@ -209,8 +208,8 @@
     } ] ],
     kdocsCn: [ [ "\u91d1\u5c71\u6587\u6863", "kdocs.cn", {
       autojump: {
-        validator: function(e) {
-          return "/office/link" === e.pathname;
+        validator: function(t) {
+          return "/office/link" === t.pathname;
         }
       }
     } ] ],
@@ -221,12 +220,13 @@
     } ] ],
     m_51CtoCom: [ [ "51CTO \u535a\u5ba2", "blog.51cto.com", {
       autojump: {
-        validator: function(e) {
-          return "/transfer" === e.pathname;
+        validator: function(t) {
+          return "/transfer" === t.pathname;
         },
         separator: "?"
       }
     } ] ],
+    ngaCn: c,
     nowcoderCom: m,
     oschinaNet: [ [ "\u5f00\u6e90\u4e2d\u56fd", /^(?:my\.)?oschina\.net$/, {
       transform: {
@@ -234,8 +234,8 @@
         separator: "GoToLink?url="
       },
       autojump: {
-        validator: function(e) {
-          return "/action/GoToLink" === e.pathname;
+        validator: function(t) {
+          return "/action/GoToLink" === t.pathname;
         },
         queryName: "url"
       }
@@ -246,8 +246,8 @@
         separator: "?"
       },
       autojump: {
-        validator: function(e) {
-          return "/jump.php" === e.pathname;
+        validator: function(t) {
+          return "/jump.php" === t.pathname;
         },
         selector: "a[href]",
         separator: "?"
@@ -260,8 +260,8 @@
         selector: '[href*="sspai.com/link?target="]'
       },
       autojump: {
-        validator: function(e) {
-          return "/link" === e.pathname;
+        validator: function(t) {
+          return "/link" === t.pathname;
         }
       }
     } ] ],
@@ -271,8 +271,8 @@
         queryName: "target"
       },
       autojump: {
-        validator: function(e) {
-          return "/developer/tools/blog-entry" === e.pathname;
+        validator: function(t) {
+          return "/developer/tools/blog-entry" === t.pathname;
         }
       }
     } ] ],
@@ -283,8 +283,8 @@
       }
     } ], [ , "weibo.cn", {
       autojump: {
-        validator: function(e) {
-          return "/sinaurl" === e.pathname;
+        validator: function(t) {
+          return "/sinaurl" === t.pathname;
         },
         queryName: "u"
       }
@@ -295,16 +295,16 @@
         queryName: "q"
       },
       autojump: {
-        validator: function(e) {
-          return "/redirect" === e.pathname;
+        validator: function(t) {
+          return "/redirect" === t.pathname;
         },
         queryName: "q"
       }
     } ] ],
     yuqueCom: [ [ "\u8bed\u96c0", "yuque.com", {
       autojump: {
-        validator: function(e) {
-          return "/r/goto" === e.pathname;
+        validator: function(t) {
+          return "/r/goto" === t.pathname;
         },
         queryName: "url"
       }
@@ -317,86 +317,89 @@
       autojump: {}
     } ] ]
   });
-  function d(e, t) {
-    (null == t || t > e.length) && (t = e.length);
-    for (var r = 0, o = new Array(t); r < t; r++) o[r] = e[r];
-    return o;
+  function d(t, e) {
+    (null == e || e > t.length) && (e = t.length);
+    for (var r = 0, n = new Array(e); r < e; r++) n[r] = t[r];
+    return n;
   }
-  function p(e, t) {
-    return function(e) {
-      if (Array.isArray(e)) return e;
-    }(e) || function(e, t) {
-      var r = null == e ? null : "undefined" != typeof Symbol && e[Symbol.iterator] || e["@@iterator"];
+  function p(t, e) {
+    return function(t) {
+      if (Array.isArray(t)) return t;
+    }(t) || function(t, e) {
+      var r = null == t ? null : "undefined" != typeof Symbol && t[Symbol.iterator] || t["@@iterator"];
       if (null != r) {
-        var o, n, a = [], u = !0, i = !1;
+        var n, o, a = [], u = !0, i = !1;
         try {
-          for (r = r.call(e); !(u = (o = r.next()).done) && (a.push(o.value), !t || a.length !== t); u = !0) ;
-        } catch (e) {
-          i = !0, n = e;
+          for (r = r.call(t); !(u = (n = r.next()).done) && (a.push(n.value), !e || a.length !== e); u = !0) ;
+        } catch (t) {
+          i = !0, o = t;
         } finally {
           try {
             u || null == r.return || r.return();
           } finally {
-            if (i) throw n;
+            if (i) throw o;
           }
         }
         return a;
       }
-    }(e, t) || function(e, t) {
-      if (!e) return;
-      if ("string" == typeof e) return d(e, t);
-      var r = Object.prototype.toString.call(e).slice(8, -1);
-      "Object" === r && e.constructor && (r = e.constructor.name);
+    }(t, e) || function(t, e) {
+      if (!t) return;
+      if ("string" == typeof t) return d(t, e);
+      var r = Object.prototype.toString.call(t).slice(8, -1);
+      "Object" === r && t.constructor && (r = t.constructor.name);
       if ("Map" === r || "Set" === r) return Array.from(r);
-      if ("Arguments" === r || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(r)) return d(e, t);
-    }(e, t) || function() {
+      if ("Arguments" === r || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(r)) return d(t, e);
+    }(t, e) || function() {
       throw new TypeError("Invalid attempt to destructure non-iterable instance.\\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");
     }();
   }
-  var h = Object.values(f).flat().find((function(e) {
-    var t = p(e, 2)[1];
-    return r(t) ? t === o : t.test(o);
+  var h = function() {
+    var t = arguments.length > 0 && void 0 !== arguments[0] ? arguments[0] : location.hostname;
+    return n ? t.replace(/^www\./, "") : "";
+  }(), v = Object.values(f).flat().find((function(t) {
+    var e = p(t, 2)[1];
+    return r(e) ? e === h : e.test(h);
   }));
-  if (e(h)) {
-    var v = h[2], g = v.transform, b = v.rewriteWindowOpen, y = v.autojump;
-    if (g) {
-      var j = g.selector, w = g.queryName, q = g.separator, k = void 0 === q ? "?target=" : q, C = g.customTransform, N = void 0 === C ? function(e) {
-        var t = w ? new URL(e.href).searchParams.get(w) : e.href.split(k)[1];
-        t && (e.href = decodeURIComponent(t));
-      } : C;
+  if (t(v)) {
+    var g = v[2], b = g.transform, y = g.rewriteWindowOpen, j = g.autojump;
+    if (b) {
+      var w = b.selector, q = b.queryName, k = b.separator, C = void 0 === k ? "?target=" : k, N = b.customTransform, A = void 0 === N ? function(t) {
+        var e = q ? new URL(t.href).searchParams.get(q) : t.href.split(C)[1];
+        e && (t.href = decodeURIComponent(e));
+      } : N;
       new MutationObserver((function() {
-        document.querySelectorAll(j).forEach(N);
+        document.querySelectorAll(w).forEach(A);
       })).observe(document.body, {
         childList: !0,
         subtree: !0
       });
     }
-    if (b) {
-      var A = b.validationRule, S = b.getOriginalUrl, U = b.separator, O = b.queryName, R = void 0 === O ? "target" : O, L = window.open;
-      window.open = function(e, o, a) {
-        if (r(e)) {
-          if (r(A) && !e.includes(A) || t(A) && !A(e)) return L.call(this, e, o, a);
-          if (t(S)) {
-            var u = S(e);
-            u && n(u) && (e = u);
+    if (y) {
+      var S = y.validationRule, U = y.getOriginalUrl, O = y.separator, R = y.queryName, L = void 0 === R ? "target" : R, T = window.open;
+      window.open = function(t, n, a) {
+        if (r(t)) {
+          if (r(S) && !t.includes(S) || e(S) && !S(t)) return T.call(this, t, n, a);
+          if (e(U)) {
+            var u = U(t);
+            u && o(u) && (t = u);
           } else {
-            var i, c = new URL(e).search;
-            e = decodeURIComponent(U ? null === (i = c.split(U)) || void 0 === i ? void 0 : i[1] : new URLSearchParams(c).get(R) || "");
+            var i, c = new URL(t).search;
+            t = decodeURIComponent(O ? null === (i = c.split(O)) || void 0 === i ? void 0 : i[1] : new URLSearchParams(c).get(L) || "");
           }
         }
-        return L.call(this, e, o, a);
+        return T.call(this, t, n, a);
       };
     }
-    y && function() {
-      var e, r = y.validator, o = y.getOriginalUrl, a = y.selector, u = y.separator, i = y.queryName, c = void 0 === i ? "target" : i;
+    j && function() {
+      var t, r = j.validator, n = j.getOriginalUrl, a = j.selector, u = j.separator, i = j.queryName, c = void 0 === i ? "target" : i;
       if (!r || r(location)) {
-        if (t(o)) {
-          var m = o();
-          if (m && n(m)) return location.replace(m);
+        if (e(n)) {
+          var m = n();
+          if (m && o(m)) return location.replace(m);
         }
         if (a && document.querySelector(a)) return document.querySelector(a).click();
-        var l = location.search, s = decodeURIComponent(u ? null === (e = l.split(u)) || void 0 === e ? void 0 : e[1] : new URLSearchParams(l).get(c) || "");
-        n(s) && location.replace(s);
+        var l = location.search, s = decodeURIComponent(u ? null === (t = l.split(u)) || void 0 === t ? void 0 : t[1] : new URLSearchParams(l).get(c) || "");
+        o(s) && location.replace(s);
       }
     }();
   }
