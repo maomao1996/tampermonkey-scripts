16d2311f81f67f7b5d33ce928b30fa2ad7579ebb
maomao <1714487678@qq.com>
2024-04-13 16:18:32 +0800

    feat(remove-redirect): 适配 Google 重定向页

---
+++ .changeset/cold-taxis-develop.md
@@ -0,0 +1,5 @@
+---
+'remove-redirect': minor
+---
+
+适配 Google 重定向页
--- packages/remove-redirect/src/sites/google.com.ts
+++ packages/remove-redirect/src/sites/google.com.ts
@@ -1,13 +1,13 @@
 import { validateUrl } from '@femm/shared-utils'
 
-/******************************************************************************
- ** 谷歌搜索
- **   - https://www.google.com/search?q=mmPlayer
- **   - https://www.google.com/search?q=茂茂物语
- ******************************************************************************/
 const sites: SiteModule = [
+  /******************************************************************************
+   ** Google 搜索
+   **   - https://www.google.com/search?q=mmPlayer
+   **   - https://www.google.com/search?q=茂茂物语
+   ******************************************************************************/
   [
-    '谷歌搜索',
+    'Google 搜索',
     /^google\.com/,
     {
       transform: {
@@ -26,6 +26,22 @@ const sites: SiteModule = [
             }
           }
         },
+      },
+    },
+  ],
+  /******************************************************************************
+   ** Google 重定向页
+   **   - https://www.google.com/url?q=https%3A%2F%2Fgithub.com%2Fmaomao1996%2Ftampermonkey-scripts&sa=D&source=docs
+   **   - https://www.google.com.hk/url?q=https%3A%2F%2Fgithub.com%2Fmaomao1996%2Ftampermonkey-scripts&sa=D&source=docs
+   **   - https://www.google.co.jp/url?q=https%3A%2F%2Fgithub.com%2Fmaomao1996%2Ftampermonkey-scripts&sa=D&source=docs
+   ******************************************************************************/
+  [
+    'Google 重定向页',
+    /^google\.(com|com?\.[a-z]{2}|[a-z]{2})$/,
+    {
+      autojump: {
+        validator: ({ pathname }) => pathname === '/url',
+        queryName: 'q',
       },
     },
   ],
