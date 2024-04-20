a238fa26aee4bc84def32aa0f6220b88767375bd
maomao <1714487678@qq.com>
2024-04-20 23:02:48 +0800

    feat(kill-watermark): 适配爱奇艺二级页面右上角 logo 移除 (#20)

---
+++ .changeset/fluffy-flies-bow.md
@@ -0,0 +1,5 @@
+---
+'kill-watermark': minor
+---
+
+适配爱奇艺二级页面右上角 logo 移除 [#20](https://github.com/maomao1996/tampermonkey-scripts/issues/20)
--- packages/kill-watermark/src/sites/iqiyi.com/index.ts
+++ packages/kill-watermark/src/sites/iqiyi.com/index.ts
@@ -1,13 +1,16 @@
 /*
  * 爱奇艺播放页
- * https://www.iqiyi.com/v_xkt6z3z798.html
+ *  - https://www.iqiyi.com/v_xkt6z3z798.html
+ *  - https://sports.iqiyi.com
+ *  - https://life.iqiyi.com
+ *  - https://yule.iqiyi.com
  ******************************************************************************/
 
 import style from './index.css'
 
 const site: SiteModule = [
   '爱奇艺播放页（右上角 logo、暂停时的广告）',
-  'iqiyi.com',
+  /^(?:[^.]+\.)?iqiyi\.com$/,
   {
     style,
   },
