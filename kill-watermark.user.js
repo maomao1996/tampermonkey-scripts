8905736269403c35bf59b29b5737f43393e0525f
maomao1996 <1714487678@qq.com>
2024-04-20 15:04:48 +0000

    Deploying to gh-pages from @ maomao1996/tampermonkey-scripts@ea717f19cf29ec3d5cb9b0da32120c3cf591f445 🚀

--- kill-watermark.user.js
+++ kill-watermark.user.js
@@ -2,14 +2,14 @@
 // @name        杀死水印（Kill Watermark）
 // @description 杀死水印（移除烦人的水印，还你一个干净清爽的页面）；已适配360 智脑、腾讯文档、飞书、FreeBuf 网络安全行业门户、稿定设计、爱奇艺播放页（右上角 logo、暂停时的广告）、腾讯课堂播放页漂浮水印、哔哩哔哩直播左上角 logo、CSDN C 知道、腾讯视频播放页（右上角 logo、暂停时的弹窗广告）、优酷视频播放页（右上角 logo、暂停时的弹窗广告）、语雀
 // @namespace   maomao1996.kill-watermark
-// @version     0.9.2
+// @version     0.10.0
 // @author      maomao1996
 // @homepage    https://github.com/maomao1996/tampermonkey-scripts
 // @supportURL  https://github.com/maomao1996/tampermonkey-scripts/issues
 // @license     MIT
 // @match       *://*.gaoding.com/*
 // @match       *://v.qq.com/x/cover/*
-// @match       *://www.iqiyi.com/*
+// @match       *://*.iqiyi.com/*
 // @match       *://v.youku.com/*
 // @match       *://live.bilibili.com/*
 // @match       *://ke.qq.com/course/*
@@ -38,19 +38,19 @@
     }
   } ], i = [ "\u7a3f\u5b9a\u8bbe\u8ba1", "gaoding.com", {
     style: '.editor-canvas div[style*="pointer-events: none !important"][style*="visibility: visible !important"][style*=background]{clip-path:circle(0)!important}'
-  } ], a = [ "\u7231\u5947\u827a\u64ad\u653e\u9875\uff08\u53f3\u4e0a\u89d2 logo\u3001\u6682\u505c\u65f6\u7684\u5e7f\u544a\uff09", "iqiyi.com", {
-    style: ".iqp-logo-bottom,.iqp-logo-box,.iqp-logo-top,iqpdiv.flash-max{display:none!important}iqpdiv.iqp-player-videolayer{height:100%!important;left:auto!important;top:auto!important;width:100%!important}"
+  } ], a = [ "\u7231\u5947\u827a\u64ad\u653e\u9875\uff08\u53f3\u4e0a\u89d2 logo\u3001\u6682\u505c\u65f6\u7684\u5e7f\u544a\uff09", /^(?:[^.]+\.)?iqiyi\.com$/, {
+    style: ".iqp-logo-bottom,.iqp-logo-box,.iqp-logo-top,.zpc-watermark,iqpdiv.flash-max{display:none!important}iqpdiv.iqp-player-videolayer{height:100%!important;left:auto!important;top:auto!important;width:100%!important}"
   } ], l = [ "\u817e\u8baf\u8bfe\u5802\u64ad\u653e\u9875\u6f02\u6d6e\u6c34\u5370", "ke.qq.com", {
     style: '#loki-player div[style*="position: absolute;"]{display:none!important}'
   } ], s = [ "\u54d4\u54e9\u54d4\u54e9\u76f4\u64ad\u5de6\u4e0a\u89d2 logo", "live.bilibili.com", {
     style: ".web-player-icon-roomStatus{opacity:0!important}"
   } ], p = [ "CSDN C \u77e5\u9053", "so.csdn.net", {
     style: ".username_mask_cover[style]{display:none!important}"
-  } ], u = [ "\u817e\u8baf\u89c6\u9891\u64ad\u653e\u9875\uff08\u53f3\u4e0a\u89d2 logo\u3001\u6682\u505c\u65f6\u7684\u5f39\u7a97\u5e7f\u544a\uff09", "v.qq.com", {
+  } ], c = [ "\u817e\u8baf\u89c6\u9891\u64ad\u653e\u9875\uff08\u53f3\u4e0a\u89d2 logo\u3001\u6682\u505c\u65f6\u7684\u5f39\u7a97\u5e7f\u544a\uff09", "v.qq.com", {
     style: "txpdiv.txp-watermark{opacity:0!important}[data-role=creative-player-pause-layer]{display:none!important}"
-  } ], y = [ "\u4f18\u9177\u89c6\u9891\u64ad\u653e\u9875\uff08\u53f3\u4e0a\u89d2 logo\u3001\u6682\u505c\u65f6\u7684\u5f39\u7a97\u5e7f\u544a\uff09", "v.youku.com", {
+  } ], u = [ "\u4f18\u9177\u89c6\u9891\u64ad\u653e\u9875\uff08\u53f3\u4e0a\u89d2 logo\u3001\u6682\u505c\u65f6\u7684\u5f39\u7a97\u5e7f\u544a\uff09", "v.youku.com", {
     style: "watermark-layer{opacity:0!important}#youku-pause-container{display:none!important}"
-  } ], c = [ "\u8bed\u96c0", "yuque.com", {
+  } ], y = [ "\u8bed\u96c0", "yuque.com", {
     style: "#main>div.wm{display:none!important}"
   } ], m = Object.freeze({
     __proto__: null,
@@ -63,9 +63,9 @@
     keQqCom: l,
     liveBilibiliCom: s,
     soCsdnNet: p,
-    vQqCom: u,
-    vYoukuCom: y,
-    yuqueCom: c
+    vQqCom: c,
+    vYoukuCom: u,
+    yuqueCom: y
   });
   function d(t, e) {
     (null == e || e > t.length) && (e = t.length);
