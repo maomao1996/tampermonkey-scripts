300c0dbfd206a9a6043c32a5f7e0e03ddafab33d
maomao <1714487678@qq.com>
2024-05-18 20:12:28 +0800

    feat(remove-redirect): 适配推特（Twitter）新域名

---
+++ .changeset/chilly-masks-wait.md
@@ -0,0 +1,5 @@
+---
+'remove-redirect': patch
+---
+
+适配推特（Twitter）新域名
--- packages/remove-redirect/src/sites/twitter.com.ts
+++ packages/remove-redirect/src/sites/twitter.com.ts
@@ -4,11 +4,13 @@ import { validateUrl } from '@femm/shared-utils'
  ** Twitter
  **   - https://twitter.com
  **   - https://twitter.com/vuejs
+ **   - https://x.com
+ **   - https://x.com/vuejs
  ******************************************************************************/
 const sites: SiteModule = [
   [
     '推特（Twitter）',
-    'twitter.com',
+    /^(twitter|x)\.com$/,
     {
       transform: {
         selector: 'a[href*="://t.co/"]',
