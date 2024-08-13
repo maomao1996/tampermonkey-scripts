b13fa4211a98fa943515a14bd82735940be1ccb6
maomao <1714487678@qq.com>
2024-08-13 22:51:19 +0800

    build(remove-redirect): 更新 `update-description` 脚本

--- packages/remove-redirect/scripts/update-description.ts
+++ packages/remove-redirect/scripts/update-description.ts
@@ -12,6 +12,6 @@ const description = Object.values(sites)
 
 const filepath = path.join(process.cwd(), 'metablock.json')
 const metablock = fs.readJSONSync(filepath)
-metablock.description = `修复跳转链接为站外直链（移除重定向），免去拦截页面点击步骤可直达站外；拦截页面自动跳转；已适配${description}`
+metablock.description = `提升用户体验：修复跳转链接为站外直链（移除重定向直接跳转），免去拦截页面点击步骤可直达站外；拦截页面自动跳转（无须额外操作）；已适配${description}`
 
 fs.writeJSONSync(filepath, metablock, { spaces: 2 })
