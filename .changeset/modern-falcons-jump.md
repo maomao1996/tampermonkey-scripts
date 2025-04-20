d461fc99965cf919d85c5429d583e7b3d0fccd16
maomao <1714487678@qq.com>
2025-04-20 17:06:37 +0800

    feat(remove-redirect): 适配 Instagram 链接转换

---
+++ .changeset/modern-falcons-jump.md
@@ -0,0 +1,5 @@
+---
+'remove-redirect': minor
+---
+
+适配 Instagram 链接转换
--- packages/remove-redirect/src/sites/instagram.com.ts
+++ packages/remove-redirect/src/sites/instagram.com.ts
@@ -1,5 +1,6 @@
 /******************************************************************************
  ** Instagram
+ **   - https://www.instagram.com/jaychou
  **   - https://www.instagram.com/linkshim/?u=https%3A%2F%2Fgithub.com%2Fmaomao1996%2Ftampermonkey-scripts&e=AT3Mdb540GAp6eHsU0Z_QGrlAc5-VgTKBSWUnq0lwzTDCfOTdT-GR2LIFutHYByEP2A0ql_mLpeU9oe1IuxVdisH-sh2AO2xwj6br5GuXjDODuX8zrwUIjYbkiya3t61XE3p3io
  **   - https://l.instagram.com/?u=https%3A%2F%2Fgithub.com%2Fmaomao1996%2Ftampermonkey-scripts&e=AT3Mdb540GAp6eHsU0Z_QGrlAc5-VgTKBSWUnq0lwzTDCfOTdT-GR2LIFutHYByEP2A0ql_mLpeU9oe1IuxVdisH-sh2AO2xwj6br5GuXjDODuX8zrwUIjYbkiya3t61XE3p3io
  ******************************************************************************/
@@ -8,6 +9,14 @@ const sites: SiteModule = [
     'Instagram',
     /^(?:l\.)?instagram\.com$/,
     {
+      transform: {
+        selector: '[href*="l.instagram.com/?u="]',
+        queryName: 'u',
+      },
+      rewriteWindowOpen: {
+        validationRule: 'l.instagram.com/?u=',
+        queryName: 'u',
+      },
       autojump: {
         validator: ({ search }) => search.includes('u='),
         queryName: 'u',
