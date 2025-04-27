1505f4bcfa55dcef6e11604d71ecfb62f8d454d3
maomao1996 <1714487678@qq.com>
2025-04-27 06:17:14 +0000

    Deploying to gh-pages from @ maomao1996/tampermonkey-scripts@d2b422c7465755d879a9adccd57fafd535bc7a11 🚀

--- kill-watermark.user.js
+++ kill-watermark.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name        杀死水印（Kill Watermark）
-// @description 杀死水印（移除烦人的水印，还你一个干净清爽的页面）；已适配360 智脑、腾讯文档、飞书、FreeBuf 网络安全行业门户、爱奇艺播放页（右上角 logo、暂停时的广告）、腾讯课堂播放页漂浮水印、哔哩哔哩直播左上角 logo、金山文档、CSDN C 知道、腾讯视频播放页（右上角 logo、暂停时的弹窗广告）、优酷视频播放页（右上角 logo、暂停时的弹窗广告）、语雀
+// @description 杀死水印（移除烦人的水印，还你一个干净清爽的页面）；已适配360 智脑、腾讯文档、飞书、FreeBuf 网络安全行业门户、爱奇艺播放页（右上角 logo、暂停时的广告）、金山文档、腾讯课堂播放页漂浮水印、哔哩哔哩直播（左上角 logo、马赛克模块）、CSDN C 知道、腾讯视频播放页（右上角 logo、暂停时的弹窗广告）、优酷视频播放页（右上角 logo、暂停时的弹窗广告）、语雀
 // @namespace   maomao1996.kill-watermark
-// @version     0.11.1
+// @version     0.12.0
 // @author      maomao1996
 // @homepage    https://github.com/maomao1996/tampermonkey-scripts
 // @supportURL  https://github.com/maomao1996/tampermonkey-scripts/issues
@@ -43,8 +43,8 @@
     style: ".uikit-watermark-container,.wo-public-watermark{display:none!important}"
   } ], site$5 = [ "\u817e\u8baf\u8bfe\u5802\u64ad\u653e\u9875\u6f02\u6d6e\u6c34\u5370", "ke.qq.com", {
     style: '#loki-player div[style*="position: absolute;"]{display:none!important}'
-  } ], site$4 = [ "\u54d4\u54e9\u54d4\u54e9\u76f4\u64ad\u5de6\u4e0a\u89d2 logo", "live.bilibili.com", {
-    style: ".web-player-icon-roomStatus{opacity:0!important}"
+  } ], site$4 = [ "\u54d4\u54e9\u54d4\u54e9\u76f4\u64ad\uff08\u5de6\u4e0a\u89d2 logo\u3001\u9a6c\u8d5b\u514b\u6a21\u5757\uff09", "live.bilibili.com", {
+    style: "#web-player-module-area-mask-panel,.web-player-icon-roomStatus{opacity:0!important}"
   } ], site$3 = [ "CSDN C \u77e5\u9053", "so.csdn.net", {
     style: ".username_mask_cover[style]{display:none!important}"
   } ], site$2 = [ "\u817e\u8baf\u89c6\u9891\u64ad\u653e\u9875\uff08\u53f3\u4e0a\u89d2 logo\u3001\u6682\u505c\u65f6\u7684\u5f39\u7a97\u5e7f\u544a\uff09", "v.qq.com", {
