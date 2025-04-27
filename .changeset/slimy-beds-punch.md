b70ab575db713763f6dc035322913f6fe1132d74
maomao <1714487678@qq.com>
2025-04-27 14:15:01 +0800

    feat(kill-watermark): 适配哔哩哔哩直播页马赛克模块 (#38)

---
+++ .changeset/slimy-beds-punch.md
@@ -0,0 +1,5 @@
+---
+'kill-watermark': minor
+---
+
+适配哔哩哔哩直播页马赛克模块 [#38](https://github.com/maomao1996/tampermonkey-scripts/issues/38)
--- packages/kill-watermark/src/sites/live.bilibili.com/index.ts
+++ packages/kill-watermark/src/sites/live.bilibili.com/index.ts
@@ -6,7 +6,7 @@
 import style from './index.css'
 
 const site: SiteModule = [
-  '哔哩哔哩直播左上角 logo',
+  '哔哩哔哩直播（左上角 logo、马赛克模块）',
   'live.bilibili.com',
   {
     style,
