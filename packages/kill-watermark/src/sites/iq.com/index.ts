26035fa34f62b61ddceb874656f378665987d403
maomao <1714487678@qq.com>
2025-09-26 22:42:19 +0800

    feat(kill-watermark): 适配爱奇艺国际版 (#43)

---
+++ packages/kill-watermark/src/sites/iq.com/index.ts
@@ -0,0 +1,16 @@
+/*
+ * 爱奇艺国际版
+ *  - https://www.iq.com
+ ******************************************************************************/
+
+import style from './index.css'
+
+const site: SiteModule = [
+  '爱奇艺国际版播放页右上角 logo',
+  'iq.com',
+  {
+    style,
+  },
+]
+
+export default site
